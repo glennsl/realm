e5c439aaadabe7b77c82a30146a88c358ce98788
glennsl <glenn@slotte.net>
2019-04-14 20:20:44 +0200

    convert simple examples to OCaml

---
+++ examples/Stateful.ml
@@ -0,0 +1,22 @@
+open! Realm.Core
+open Realm.React
+module App = SimpleApp(struct
+  type model =
+    { count: int }
+
+  let init () =
+    { count = 0 }
+
+  let click =
+    Effect.update (fun model  -> { count = (model.count + 1) })
+
+  let view model =
+    let open Html in
+    let open Attr in
+    let message =
+      "You've clicked this " ^ String.fromInt model.count ^ " times(s)"
+      in
+    div
+      [ button ~attrs:[ onClick click ] [ text message ]
+      ]
+end)
